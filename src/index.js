8b561e2ab1d189a93bf090258e245fb46059fc61
lubin <lu.bin.lu.bin@gmail.com>
2018-12-01 22:44:49 -0500

    add redux-observable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { connect, Provider } from 'react-redux';
+import { delay, mapTo } from 'rxjs/operators';
+
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const PING = 'PING';
+const PONG = 'PONG';
+
+const ping = () => ({ type: PING });
+
+const pingEpic = action$ =>
+  action$.ofType(PING).pipe(
+    delay(1000), // Asynchronously wait 1000ms then continue
+    mapTo({ type: PONG })
+  );
+
+const pingReducer = (state = { isPinging: false }, action) => {
+  switch (action.type) {
+    case PING:
+      return { isPinging: true };
+
+    case PONG:
+      return { isPinging: false };
+
+    default:
+      return state;
+  }
+};
+
+// components/App.js
+
+let App = ({ isPinging, ping }) => (
+  <div>
+    <h1>is pinging: {isPinging.toString()}</h1>
+    <button onClick={ping}>Start PING</button>
+  </div>
+);
+
+App = connect(
+  ({ isPinging }) => ({ isPinging }),
+  { ping }
+)(App);
+
+// redux/configureStore.js
+
+const epicMiddleware = createEpicMiddleware();
+
+const store = createStore(pingReducer,
+  applyMiddleware(epicMiddleware)
+);
+
+epicMiddleware.run(pingEpic);
+
+// index.js
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
