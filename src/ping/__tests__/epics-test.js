39478a74661e2234c46486666847da7d9a71c5dc
Bin Lu <lu.bin.lu.bin@gmail.com>
2018-12-05 15:31:43 -0500

    test epic/observable by marbles

---
+++ src/ping/__tests__/epics-test.js
@@ -0,0 +1,38 @@
+import { throttleTime } from 'rxjs/operators';
+import { TestScheduler } from 'rxjs/testing';
+
+import { pingEpic } from '../epics';
+import { PING, PONG } from '../actions';
+
+let scheduler;
+beforeEach(() => {
+  scheduler = new TestScheduler((actual, expected) => {
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('pingEpic', () => {
+  it('should generate the stream correctly', () => {
+    scheduler.run(helpers => {
+      const { cold, expectObservable, expectSubscriptions } = helpers;
+      const action$ =  cold('-a 1s a-|', { a: {type: PING}});
+      // const subs =     '^--!';
+      const expected = '- 1s a 1s a-|';
+      const output$ = pingEpic(action$);
+      expectObservable(output$).toBe(expected, {a: {type: PONG}});
+      // expectSubscriptions(action$.subscriptions).toBe(subs);
+    });   
+  });
+
+  it('generate the stream correctly 2', () => {
+    scheduler.run(helpers => {
+      const { cold, expectObservable, expectSubscriptions } = helpers;
+      const e1 =  cold('-a--b--c---|');
+      const subs =     '^----------!';
+      const expected = '-a-----c---|';
+  
+      expectObservable(e1.pipe(throttleTime(3))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(subs);
+    });
+  });  
+});
--- src/ping/epics.js
+++ src/ping/epics.js
@@ -1,8 +1,9 @@
-import { delay, mapTo } from 'rxjs/operators';
+import { delay, filter, mapTo } from 'rxjs/operators';
 
 import { PING, pong } from './actions';
 
-export const pingEpic = action$ => action$.ofType(PING).pipe(
+export const pingEpic = action$ => action$.pipe(
+  filter(v => v.type === PING),
   delay(1000), // Asynchronously wait 1000ms then continue
   mapTo(pong())
 );
