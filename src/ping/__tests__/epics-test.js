eb7f5be9156cb43d6a4ee46fca3e77c97a0b9f8d
Bin Lu <lu.bin.lu.bin@gmail.com>
2018-12-07 13:54:33 -0500

    marbles: error and sync group

--- src/ping/__tests__/epics-test.js
+++ src/ping/__tests__/epics-test.js
@@ -15,13 +15,37 @@ describe('pingEpic', () => {
   it('should generate the stream correctly', () => {
     scheduler.run(helpers => {
       const { cold, expectObservable, expectSubscriptions } = helpers;
-      const action$ =  cold('-a 1s a-|', { a: {type: PING}});
-      // const subs =     '^--!';
-      const expected = '- 1s a 1s a-|';
+      const action$ =  cold('-a 1s  a   -|', { a: {type: PING}});
+      const subs =          '^- 1s  -   -!';
+      const expected = '     - 1s a 1s a-|';      
       const output$ = pingEpic(action$);
       expectObservable(output$).toBe(expected, {a: {type: PONG}});
-      // expectSubscriptions(action$.subscriptions).toBe(subs);
-    });   
+      expectSubscriptions(action$.subscriptions).toBe(subs);
+    });    
+  });
+
+  it('should generate error correctly', () => {
+    scheduler.run(helpers => {
+      const { cold, expectObservable, expectSubscriptions } = helpers;
+      const action$ =  cold('-a 1s  #   -|', { a: {type: PING}});
+      const subs =          '^- 1s  !';
+      const expected = '     - 1s a #';      
+      const output$ = pingEpic(action$);
+      expectObservable(output$).toBe(expected, {a: {type: PONG}});
+      expectSubscriptions(action$.subscriptions).toBe(subs);
+    });    
+  });
+
+  it('should generate sync group correctly', () => {
+    scheduler.run(helpers => {
+      const { cold, expectObservable, expectSubscriptions } = helpers;
+      const action$ =  cold('-(aaa) 1s  a   -|', { a: {type: PING}});
+      const subs =          '^----- 1s  -   -!';
+      const expected = '     - 1s (aaa) 1s a-|';      
+      const output$ = pingEpic(action$);
+      expectObservable(output$).toBe(expected, {a: {type: PONG}});
+      expectSubscriptions(action$.subscriptions).toBe(subs);
+    });    
   });
 
   it('generate the stream correctly 2', () => {
